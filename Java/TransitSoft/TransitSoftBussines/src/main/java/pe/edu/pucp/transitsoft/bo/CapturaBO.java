b2d4cc50bc3c60dd9ab13e7508c10fc6237ed2d1
Kenyi <a20231466@pucp.edu.pe>
2025-10-16 21:50:16 -0500

    aa

--- Java/TransitSoft/TransitSoftBussines/src/main/java/pe/edu/pucp/transitsoft/bo/CapturaBO.java
+++
@@ -1,9 +0,0 @@
-package pe.edu.pucp.transitsoft.bo;
-
-import java.util.List;
-import pe.edu.pucp.transitsoft.model.CapturaDTO;
-
-public interface CapturaBO {
-    List<CapturaDTO> obtenerCapturasConExcesoDeVelocidad();
-    void actualizar(CapturaDTO modelo);
-}
--- Java/TransitSoft/TransitSoftPersistance/src/main/java/pe/edu/pucp/transitsoft/daoimpl/util/Tipo_Operacion.java
+++
@@ -1,5 +0,0 @@
-package pe.edu.pucp.transitsoft.daoimpl.util;
-
-public enum Tipo_Operacion {
-    INSERTAR, MODIFICAR, ELIMINAR
-}
